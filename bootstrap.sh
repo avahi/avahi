8ae3b21733c5fc8ae74a413cb7707eff66120c4f
Lennart Poettering <lennart@poettering.net>
2005-08-29 12:41:27 +0000

    use exec for starting autogen.sh from bootstrap.sh
    git-svn-id: file:///home/lennart/svn/public/avahi/trunk@492 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,4 +18,4 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA.
 
-CFLAGS="$CFLAGS -g -O0" ./autogen.sh --sysconfdir=/etc --localstatedir=/var "$@"
+CFLAGS="$CFLAGS -g -O0" exec ./autogen.sh --sysconfdir=/etc --localstatedir=/var "$@"

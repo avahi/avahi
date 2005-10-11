6daf938592d243f52281a6933f2ed04af937fc1f
Lennart Poettering <lennart@poettering.net>
2005-10-11 01:24:53 +0000

    add an example for subtype usage
    git-svn-id: file:///home/lennart/svn/public/avahi/trunk@722 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- examples/client-publish-service.c
+++ examples/client-publish-service.c
@@ -94,6 +94,12 @@ static void create_services(AvahiClient *c) {
         goto fail;
     }
 
+    /* Add an additional (hypothetic) subtype */
+    if ((ret = avahi_entry_group_add_service_subtype(group, AVAHI_IF_UNSPEC, AVAHI_PROTO_UNSPEC, 0, name, "_printer._tcp", NULL, "_magic._sub._printer._tcp") < 0)) {
+        fprintf(stderr, "Failed to add subtype _magic._sub._printer._tcp: %s\n", avahi_strerror(ret));
+        goto fail;
+    }
+    
     /* Tell the server to register the service */
     if ((ret = avahi_entry_group_commit(group)) < 0) {
         fprintf(stderr, "Failed to commit entry_group: %s\n", avahi_strerror(ret));

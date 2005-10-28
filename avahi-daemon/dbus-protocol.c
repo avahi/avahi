fd25072a95549aff47d5abafd712fb14de55e172
Sebastien Estienne <sebastien.estienne@gmail.com>
2005-10-28 23:00:04 +0000

    * more trivial fix, now avahi-daemon and dnsconfd compiles on OpenBSD
    git-svn-id: file:///home/lennart/svn/public/avahi/trunk@904 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- avahi-daemon/dbus-protocol.c
+++ avahi-daemon/dbus-protocol.c
@@ -25,6 +25,7 @@
 
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <net/if.h>
--- avahi-daemon/simple-protocol.c
+++ avahi-daemon/simple-protocol.c
@@ -25,6 +25,7 @@
 
 #include <assert.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
 #include <unistd.h>

b778941546ddd9f7922362d55c5c1261b5cedf9f
Lennart Poettering <lennart@poettering.net>
2006-01-30 15:41:54 +0000

    forward error back to the client when a commit() call fails
    git-svn-id: file:///home/lennart/svn/public/avahi/trunk@1118 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- avahi-daemon/dbus-entry-group.c
+++ avahi-daemon/dbus-entry-group.c
@@ -120,7 +120,9 @@ DBusHandlerResult avahi_dbus_msg_entry_group_impl(DBusConnection *c, DBusMessage
             goto fail;
         }
 
-        avahi_s_entry_group_commit(i->entry_group);
+        if (avahi_s_entry_group_commit(i->entry_group) < 0)
+            return avahi_dbus_respond_error(c, m, avahi_server_errno(avahi_server), NULL);
+        
         return avahi_dbus_respond_ok(c, m);
         
         

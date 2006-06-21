cebb8af4a847631350541b27aa1343be3fc3443a
Lennart Poettering <lennart@poettering.net>
2006-06-21 21:29:37 +0000

    if RLIMIT_NPROC is not available but used in the config file, don't fail but warn
    git-svn-id: file:///home/lennart/svn/public/avahi/trunk@1222 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- avahi-daemon/main.c
+++ avahi-daemon/main.c
@@ -594,10 +594,12 @@ static int load_config_file(DaemonConfig *c) {
                 } else if (strcasecmp(p->key, "rlimit-stack") == 0) {
                     c->rlimit_stack_set = 1;
                     c->rlimit_stack = atoi(p->value);
-#ifdef RLIMIT_NPROC
                 } else if (strcasecmp(p->key, "rlimit-nproc") == 0) {
+#ifdef RLIMIT_NPROC
                     c->rlimit_nproc_set = 1;
                     c->rlimit_nproc = atoi(p->value);
+#else
+                    avahi_log_error("Ignoring configuration key \"%s\" in group \"%s\"\n", p->key, g->name);
 #endif
                 } else {
                     avahi_log_error("Invalid configuration key \"%s\" in group \"%s\"\n", p->key, g->name);

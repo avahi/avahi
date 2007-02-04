bd68f1a8609d726a46e4487d016427efbc815da1
Lennart Poettering <lennart@poettering.net>
2007-02-04 17:12:18 +0000

    move assert check before access
    git-svn-id: file:///home/lennart/svn/public/avahi/trunk@1383 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- avahi-core/iface-pfroute.c
+++ avahi-core/iface-pfroute.c
@@ -262,9 +262,10 @@ static void socket_event(AvahiWatch *w, int fd, AVAHI_GCC_UNUSED AvahiWatchEvent
 
 int avahi_interface_monitor_init_osdep(AvahiInterfaceMonitor *m) {
     int fd = -1;
-    m->osdep.pfroute = NULL;
 
     assert(m);
+
+    m->osdep.pfroute = NULL;
 
     if ((fd = socket(PF_ROUTE, SOCK_RAW, AF_UNSPEC)) < 0) {
         avahi_log_error(__FILE__": socket(PF_ROUTE): %s", strerror(errno));

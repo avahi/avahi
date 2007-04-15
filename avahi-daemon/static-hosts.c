86d765aea50ec95b57263497bd82bcc485ac6b3c
Lennart Poettering <lennart@poettering.net>
2007-04-15 00:35:11 +0000

    Cleanup static services subsystem: properly notice when static hosts are removed or are added, other fixes
    git-svn-id: file:///home/lennart/svn/public/avahi/trunk@1427 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- avahi-daemon/static-hosts.c
+++ avahi-daemon/static-hosts.c
@@ -40,13 +40,16 @@ typedef struct StaticHost StaticHost;
 
 struct StaticHost {
     AvahiSEntryGroup *group;
+    int iteration;
 
-    char *host, *ip;
+    char *host;
+    AvahiAddress address;
 
     AVAHI_LLIST_FIELDS(StaticHost, hosts);
 };
 
 static AVAHI_LLIST_HEAD(StaticHost, hosts) = NULL;
+static int current_iteration = 0;
 
 static void add_static_host_to_server(StaticHost *h);
 static void remove_static_host_from_server(StaticHost *h);
@@ -86,7 +89,7 @@ static StaticHost *static_host_new(void) {
 
     s->group = NULL;
     s->host = NULL;
-    s->ip = NULL;
+    s->iteration = current_iteration;
 
     AVAHI_LLIST_PREPEND(StaticHost, hosts, hosts, s);
 
@@ -102,17 +105,25 @@ static void static_host_free(StaticHost *s) {
         avahi_s_entry_group_free (s->group);
 
     avahi_free(s->host);
-    avahi_free(s->ip);
     
     avahi_free(s);
 }
 
+static StaticHost *static_host_find(const char *host, const AvahiAddress *a) {
+    StaticHost *h;
+
+    assert(host);
+    assert(a);
+    
+    for (h = hosts; h; h = h->hosts_next)
+        if (!strcmp(h->host, host) && !avahi_address_cmp(a, &h->address))
+            return h;
+
+    return NULL;
+}
+
 static void add_static_host_to_server(StaticHost *h)
 {
-    AvahiAddress a;
-    AvahiProtocol p;
-    int err;
-    const AvahiServerConfig *config;
 
     if (!h->group)
         if (!(h->group = avahi_s_entry_group_new (avahi_server, entry_group_callback, h))) {
@@ -120,22 +131,22 @@ static void add_static_host_to_server(StaticHost *h)
             return;
         }
 
-    if (!avahi_address_parse (h->ip, AVAHI_PROTO_UNSPEC, &a)) {
-        avahi_log_error("Static host name %s: avahi_address_parse failed", h->host);
-        return;
-    }
+    if (avahi_s_entry_group_is_empty(h->group)) {
+        AvahiProtocol p;
+        int err;
+        const AvahiServerConfig *config;
+        config = avahi_server_get_config(avahi_server);
+        
+        p = (h->address.proto == AVAHI_PROTO_INET && config->publish_a_on_ipv6) ||
+            (h->address.proto == AVAHI_PROTO_INET6 && config->publish_aaaa_on_ipv4) ? AVAHI_PROTO_UNSPEC : h->address.proto;
+        
+        if ((err = avahi_server_add_address(avahi_server, h->group, AVAHI_IF_UNSPEC, p, 0, h->host, &h->address)) < 0) {
+            avahi_log_error ("Static host name %s: avahi_server_add_address failure: %s", h->host, avahi_strerror(err));
+            return;
+        }
 
-    config = avahi_server_get_config(avahi_server);
-    
-    p = (a.proto == AVAHI_PROTO_INET && config->publish_a_on_ipv6) ||
-        (a.proto == AVAHI_PROTO_INET6 && config->publish_aaaa_on_ipv4) ? AVAHI_PROTO_UNSPEC : a.proto;
-    
-    if ((err = avahi_server_add_address(avahi_server, h->group, AVAHI_IF_UNSPEC, p, 0, h->host, &a)) < 0) {
-        avahi_log_error ("Static host name %s: avahi_server_add_address failure: %s", h->host, avahi_strerror(err));
-        return;
+        avahi_s_entry_group_commit (h->group);
     }
-
-    avahi_s_entry_group_commit (h->group);
 }
 
 static void remove_static_host_from_server(StaticHost *h)
@@ -147,36 +158,36 @@ static void remove_static_host_from_server(StaticHost *h)
 void static_hosts_add_to_server(void) {
     StaticHost *h;
 
-    for (h = hosts; h; h = h->hosts_next) {
+    for (h = hosts; h; h = h->hosts_next)
         add_static_host_to_server(h);
-    }
 }
 
 void static_hosts_remove_from_server(void) {
     StaticHost *h;
 
-    for (h = hosts; h; h = h->hosts_next) {
+    for (h = hosts; h; h = h->hosts_next)
         remove_static_host_from_server(h);
-    }
 }
 
 void static_hosts_load(int in_chroot) {
     FILE *f;
     unsigned int line = 0;
-    const char *filename = (in_chroot ? "/hosts" : AVAHI_CONFIG_DIR "/hosts");
+    StaticHost *h, *next;
+    const char *filename = in_chroot ? "/hosts" : AVAHI_CONFIG_DIR "/hosts";
 
-    if (!(f = fopen(filename, "r")))
-    {
+    if (!(f = fopen(filename, "r"))) {
         if (errno != ENOENT)
             avahi_log_error ("Failed to open static hosts file: %s", strerror (errno));
         return;
     }
 
+    current_iteration++;
+    
     while (!feof(f)) {
         unsigned int len;
         char ln[256], *s;
         char *host, *ip;
-        StaticHost *h;
+        AvahiAddress a;
 
         if (!fgets(ln, sizeof (ln), f))
             break;
@@ -209,7 +220,7 @@ void static_hosts_load(int in_chroot) {
             avahi_log_error("%s:%d: Error, unexpected end of line!", filename, line);
             avahi_free(host);
             avahi_free(ip);
-	    break;
+            goto fail;
         }
 
         /* Skip over the host */
@@ -223,14 +234,42 @@ void static_hosts_load(int in_chroot) {
             avahi_log_error ("%s:%d: Junk on the end of the line!", filename, line);
             avahi_free(host);
             avahi_free(ip);
-            break;
+            goto fail;
         }
 
-        h = static_host_new();
-        h->host = host;
-        h->ip = ip;
+        if (!avahi_address_parse(ip, AVAHI_PROTO_UNSPEC, &a)) {
+            avahi_log_error("Static host name %s: failed to parse address %s", host, ip);
+            avahi_free(host);
+            avahi_free(ip);
+            goto fail;
+        }
+
+        avahi_free(ip);
+
+        if ((h = static_host_find(host, &a)))
+            avahi_free(host);
+        else {
+            h = static_host_new();
+            h->host = host;
+            h->address = a;
+
+            avahi_log_info("Loading new static hostname %s.", h->host);
+        }
+
+        h->iteration = current_iteration;
     }
 
+    for (h = hosts; h; h = next) {
+        next = h->hosts_next;
+        
+        if (h->iteration != current_iteration) {
+            avahi_log_info("Static hostname %s vanished, removing.", h->host);
+            static_host_free(h);
+        }
+    }
+
+fail:
+    
     fclose(f);
 }
 

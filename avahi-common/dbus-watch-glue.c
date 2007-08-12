866440b19a3be830fa77f9039d5b0202ac7bf840
Lennart Poettering <lennart@poettering.net>
2007-08-12 17:16:43 +0000

    use dbus_watch_get_unix_fd() only in dbus >= 1.1.1
    git-svn-id: file:///home/lennart/svn/public/avahi/trunk@1518 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- avahi-common/dbus-watch-glue.c
+++ avahi-common/dbus-watch-glue.c
@@ -134,7 +134,11 @@ static dbus_bool_t update_watch(const AvahiPoll *poll_api, DBusWatch *dbus_watch
 
         if (!(avahi_watch = poll_api->watch_new(
                   poll_api,
+#if (DBUS_VERSION_MAJOR == 1 && DBUS_VERSION_MINOR == 1 && DBUS_VERSION_MICRO >= 1) || (DBUS_VERSION_MAJOR == 1 && DBUS_VERSION_MAJOR > 1) || (DBUS_VERSION_MAJOR > 1)
                   dbus_watch_get_unix_fd(dbus_watch),
+#else
+                  dbus_watch_get_fd(dbus_watch),
+#endif
                   translate_dbus_to_avahi(dbus_watch_get_flags(dbus_watch)),
                   watch_callback,
                   dbus_watch)))

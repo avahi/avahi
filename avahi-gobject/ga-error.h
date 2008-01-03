7686f1b38b13a660ac14b799c62f453fa012b4d7
Sjoerd Simons <sjoerd@debian.org>
2008-01-03 15:32:59 +0000

    Remove C Nokia notice that actually remained from the copyright boilerplate
    git-svn-id: file:///home/lennart/svn/public/avahi/trunk@1671 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- avahi-gobject/ga-error.h
+++ avahi-gobject/ga-error.h
@@ -1,7 +1,6 @@
 /*
  * ga-error.h - Header for Avahi error types
  * Copyright (C) 2005 Collabora Ltd.
- * Copyright (C) 2005 Nokia Corporation
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public

1616785d583939aa4c1b859fcf626759214769dd
Trent Lloyd <trent@lloyd.id.au>
2015-03-31 09:00:09 +0800

    avahi_server_set_browse_domains: check the provided domain list, instead of the list from the configuration file

--- avahi-core/server.c
+++ avahi-core/server.c
@@ -1794,7 +1794,7 @@ int avahi_server_set_browse_domains(AvahiServer *s, AvahiStringList *domains) {
 
     assert(s);
 
-    for (l = s->config.browse_domains; l; l = l->next)
+    for (l = domains; l; l = l->next)
         if (!avahi_is_valid_domain_name((char*) l->text))
             return avahi_server_set_errno(s, AVAHI_ERR_INVALID_DOMAIN_NAME);
 

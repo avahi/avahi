ae42a04b3a1757d61990bfa8da55400e3a2bb3cb
Thomas Kluyver <takowl@gmail.com>
2015-11-13 14:49:05 +0000

    Update avahi-discover code for GTK 3 and Python 3

--- avahi-python/avahi-discover/avahi-discover.py
+++ avahi-python/avahi-discover/avahi-discover.py
@@ -17,18 +17,21 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA.
 
+from __future__ import print_function
+
 import os, sys
 
 try:
-    import avahi, gettext, gtk, gobject, dbus, avahi.ServiceTypeDatabase
     gettext.bindtextdomain(@GETTEXT_PACKAGE@, @LOCALEDIR@)
     gettext.textdomain(@GETTEXT_PACKAGE@)
+    import avahi, gettext, dbus, avahi.ServiceTypeDatabase
+    from gi.repository import Gtk, GObject
     _ = gettext.gettext
-except ImportError, e:
-    print "Sorry, to use this tool you need to install Avahi, pygtk and python-dbus.\n Error: %s" % e
+except ImportError as e:
+    print("Sorry, to use this tool you need to install Avahi, pygtk and python-dbus.\n Error: %s" % e)
     sys.exit(1)
-except Exception, e:
-    print "Failed to initialize: %s" % e
+except Exception as e:
+    print("Failed to initialize: %s" % e)
     sys.exit(1)
 
 
@@ -38,15 +41,15 @@ except Exception, e:
 try:
     from dbus import DBusException
     import dbus.glib
-except ImportError, e:
+except ImportError as e:
     pass
 
 service_type_browsers = {}
 service_browsers = {}
 
 def error_msg(msg):
-    d = gtk.MessageDialog(parent=None, flags=gtk.DIALOG_MODAL,
-                          type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK)
+    d = Gtk.MessageDialog(parent=None, flags=Gtk.DialogFlags.MODAL,
+                          type=Gtk.MessageType.ERROR, buttons=Gtk.ButtonsType.OK)
     d.set_markup(msg)
     d.show_all()
     d.run()
@@ -59,8 +62,8 @@ service_type_db = avahi.ServiceTypeDatabase.ServiceTypeDatabase()
 class Main_window:
     def __init__(self, path="avahi-discover.ui", root="main_window", domain=None, **kwargs):
         path = os.path.join(ui_dir, path)
-        gtk.window_set_default_icon_name("network-wired")
-        self.ui = gtk.Builder()
+        Gtk.Window.set_default_icon_name("network-wired")
+        self.ui = Gtk.Builder()
         self.ui.set_translation_domain("avahi")
         self.ui.add_from_file(path)
         self.ui.connect_signals(self)
@@ -99,44 +102,50 @@ class Main_window:
             return str(self.siocgifname(interface)) + " " + str(self.protoname(protocol))
 
     def service_resolved(self, interface, protocol, name, stype, domain, host, aprotocol, address, port, txt, flags):
-        print "Service data for service '%s' of type '%s' in domain '%s' on %i.%i:" % (name, stype, domain, interface, protocol)
+        print("Service data for service '%s' of type '%s' in domain '%s' on %i.%i:"
+                % (name, stype, domain, interface, protocol))
 
-        print "\tHost %s (%s), port %i, TXT data: %s" % (host, address, port, str(avahi.txt_array_to_string_array(txt)))
+        print("\tHost %s (%s), port %i, TXT data: %s"
+                % (host, address, port, str(avahi.txt_array_to_string_array(txt))))
 
         self.update_label(interface, protocol, name, stype, domain, host, aprotocol, address, port, avahi.txt_array_to_string_array(txt))
 
     def print_error(self, err):
         error_label = "<b>Error:</b> %s" % (err)
         self.info_label.set_markup(error_label)
-        print "Error:", str(err)
+        print("Error:", str(err))
 
     def lookup_type(self, stype):
         global service_type_db
 
         try:
-            return service_type_db[stype]
+            return service_type_db[stype].decode('utf-8', 'replace')
         except KeyError:
             return stype
 
     def new_service(self, interface, protocol, name, stype, domain, flags):
-        print "Found service '%s' of type '%s' in domain '%s' on %i.%i." % (name, stype, domain, interface, protocol)
-        if self.zc_ifaces.has_key((interface,protocol)) == False:
+        print("Found service '%s' of type '%s' in domain '%s' on %i.%i."
+                % (name, stype, domain, interface, protocol))
 
+        if (interface,protocol) not in self.zc_ifaces:
             ifn = self.get_interface_name(interface, protocol)
-
             self.zc_ifaces[(interface,protocol)] = self.insert_row(self.treemodel, None, ifn, None,interface,protocol,None,domain)
-        if self.zc_domains.has_key((interface,protocol,domain)) == False:
+
+        if (interface,protocol,domain) not in self.zc_domains:
             self.zc_domains[(interface,protocol,domain)] = self.insert_row(self.treemodel, self.zc_ifaces[(interface,protocol)], domain,None,interface,protocol,None,domain)
-        if self.zc_types.has_key((interface,protocol,stype,domain)) == False:
+
+        if (interface,protocol,stype,domain) not in self.zc_types:
             thisDomain = self.zc_domains[(interface,protocol,domain)]
             self.zc_types[(interface,protocol,stype,domain)] = self.insert_row(self.treemodel, thisDomain, self.lookup_type(stype), name, interface,None,None,None)
+
         treeiter = self.insert_row(self.treemodel,self.zc_types[(interface,protocol,stype,domain)], name, name, interface,protocol,stype,domain)
         self.services_browsed[(interface, protocol, name, stype, domain)] = treeiter
         # expand the tree of this path
         self.tree_view.expand_to_path(self.treemodel.get_path(treeiter))
 
     def remove_service(self, interface, protocol, name, stype, domain, flags):
-        print "Service '%s' of type '%s' in domain '%s' on %i.%i disappeared." % (name, stype, domain, interface, protocol)
+        print("Service '%s' of type '%s' in domain '%s' on %i.%i disappeared."
+                % (name, stype, domain, interface, protocol))
         self.info_label.set_markup("")
         treeiter=self.services_browsed[(interface, protocol, name, stype, domain)]
         parent = self.treemodel.iter_parent(treeiter)
@@ -162,10 +171,11 @@ class Main_window:
         global service_browsers
 
         # Are we already browsing this domain for this type?
-        if service_browsers.has_key((interface, protocol, stype, domain)):
+        if (interface, protocol, stype, domain) in service_browsers:
             return
 
-        print "Browsing for services of type '%s' in domain '%s' on %i.%i ..." % (stype, domain, interface, protocol)
+        print("Browsing for services of type '%s' in domain '%s' on %i.%i ..."
+                % (stype, domain, interface, protocol))
 
         b = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, self.server.ServiceBrowserNew(interface, protocol, stype, domain, dbus.UInt32(0))),  avahi.DBUS_INTERFACE_SERVICE_BROWSER)
         b.connect_to_signal('ItemNew', self.new_service)
@@ -177,16 +187,16 @@ class Main_window:
         global service_type_browsers
 
         # Are we already browsing this domain?
-        if service_type_browsers.has_key((interface, protocol, domain)):
+        if (interface, protocol, domain) in service_type_browsers:
             return
 
         if self.stype is None:
-            print "Browsing domain '%s' on %i.%i ..." % (domain, interface, protocol)
+            print("Browsing domain '%s' on %i.%i ..." % (domain, interface, protocol))
 
             try:
                 b = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, self.server.ServiceTypeBrowserNew(interface, protocol, domain, dbus.UInt32(0))),  avahi.DBUS_INTERFACE_SERVICE_TYPE_BROWSER)
-            except DBusException, e:
-                print e
+            except DBusException as e:
+                print(e)
                 error_msg("You should check that the avahi daemon is running.\n\nError : %s" % e)
                 sys.exit(0)
 
@@ -241,16 +251,16 @@ class Main_window:
         return myiter
 
     def new(self):
-        self.treemodel=gtk.TreeStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
+        self.treemodel=Gtk.TreeStore(GObject.TYPE_STRING, GObject.TYPE_STRING, GObject.TYPE_STRING, GObject.TYPE_STRING, GObject.TYPE_STRING, GObject.TYPE_STRING)
         self.tree_view.set_model(self.treemodel)
 
         #creating the columns headers
         self.tree_view.set_headers_visible(False)
-        renderer=gtk.CellRendererText()
-        column=gtk.TreeViewColumn("",renderer, text=0)
+        renderer=Gtk.CellRendererText()
+        column=Gtk.TreeViewColumn("",renderer, text=0)
         column.set_resizable(True)
-        column.set_sizing("GTK_TREE_VIEW_COLUMN_GROW_ONLY");
-        column.set_expand(True);
+        column.set_sizing(Gtk.TreeViewColumnSizing.GROW_ONLY)
+        column.set_expand(True)
         self.tree_view.append_column(column)
 
         self.domain = None
@@ -263,8 +273,8 @@ class Main_window:
         try:
             self.bus = dbus.SystemBus()
             self.server = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME, avahi.DBUS_PATH_SERVER), avahi.DBUS_INTERFACE_SERVER)
-        except Exception, e:
-            print "Failed to connect to Avahi Server (Is it running?): %s" % e
+        except Exception as e:
+            print("Failed to connect to Avahi Server (Is it running?): %s" % e)
             sys.exit(1)
 
         if self.domain is None:
@@ -279,11 +289,11 @@ class Main_window:
             self.browse_domain(avahi.IF_UNSPEC, avahi.PROTO_UNSPEC, domain)
 
     def gtk_main_quit(self, *args):
-        gtk.main_quit()
+        Gtk.main_quit()
 
 def main():
     main_window = Main_window()
-    gtk.main()
+    Gtk.main()
 
 if __name__ == "__main__":
     main()

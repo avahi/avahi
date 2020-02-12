0a536f6082037d07ceae1404e95fec7cd40ff20c
Trent Lloyd <trent@lloyd.id.au>
2020-02-12 16:24:45 +0800

    cleanup: ensure entries are dead for at least 1s
    If a dead AvahiEntry cleanup is scheduled, and we try to schedule
    another one, defer the existing callback by 1s. Also remove other calls
    to avahi_cleanup_dead_entries.
    
    This is done to ensure that an entry stays in the dead state for at
    least 1 second, so that if we receive a copy of our own probe we don't
    think it's a conflict.
    
    This should resolve issues where on startup the server spuriously
    conflicts with itself and renames to hostname-2, hostname-3, etc. This
    typically happens as addresses are rapidly added and removed, e.g. when
    IPv6 global addresses cause the link local address to be revoked.
    
    Ideally to prevent excess memory usage we would track the age of the
    entries and expire them sooner to prevent the schedule being deferred
    forever on a busy server.

--- avahi-core/server.c
+++ avahi-core/server.c
@@ -1054,8 +1054,6 @@ static void mcast_socket_event(AvahiWatch *w, int fd, AvahiWatchEvent events, vo
             avahi_log_error("Incoming packet received on address that isn't local.");
 
         avahi_dns_packet_free(p);
-
-        avahi_cleanup_dead_entries(s);
     }
 }
 
@@ -1077,8 +1075,6 @@ static void legacy_unicast_socket_event(AvahiWatch *w, int fd, AvahiWatchEvent e
     if (p) {
         dispatch_legacy_unicast_packet(s, p);
         avahi_dns_packet_free(p);
-
-        avahi_cleanup_dead_entries(s);
     }
 }
 

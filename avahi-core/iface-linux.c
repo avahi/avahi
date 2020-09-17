2b6bccca5d9d8ab7f11219a639707b325910a0b1
Philip Prindeville <philipp@redfish-solutions.com>
2020-09-17 00:27:55 +0000

    Logic for p2p on tunnels incorrectly requires IFF_MULTICAST (#305)
    Signed-off-by: Philip Prindeville <philipp@redfish-solutions.com>

--- avahi-core/iface-linux.c
+++ avahi-core/iface-linux.c
@@ -105,8 +105,8 @@ static void netlink_callback(AvahiNetlink *nl, struct nlmsghdr *n, void* userdat
             (ifinfomsg->ifi_flags & IFF_UP) &&
             (!m->server->config.use_iff_running || (ifinfomsg->ifi_flags & IFF_RUNNING)) &&
             ((ifinfomsg->ifi_flags & IFF_LOOPBACK) ||
-             (ifinfomsg->ifi_flags & IFF_MULTICAST)) &&
-            (m->server->config.allow_point_to_point || !(ifinfomsg->ifi_flags & IFF_POINTOPOINT));
+             (ifinfomsg->ifi_flags & IFF_MULTICAST) ||
+             ((ifinfomsg->ifi_flags & IFF_POINTOPOINT) && m->server->config.allow_point_to_point));
 
         /* Handle interface attributes */
         l = NLMSG_PAYLOAD(n, sizeof(struct ifinfomsg));

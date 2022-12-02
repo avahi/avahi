10f37e61f2b8598386243f9642f303eb2cdd87ac
Evgeny Vereshchagin <evvers@ya.ru>
2022-12-02 05:20:01 +0000

    CI: bring ASAN/UBSan

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,6 +3,8 @@
 set -eux
 set -o pipefail
 
+export ASAN_UBSAN=${ASAN_UBSAN:-false}
+
 case "$1" in
     install-build-deps)
         sed -i -e '/^#\s*deb-src.*\smain\s\+restricted/s/^#//' /etc/apt/sources.list
@@ -12,7 +14,13 @@ case "$1" in
         apt-get install -y gcc clang
         ;;
     build)
-        ./bootstrap.sh --enable-tests
+        if [[ "$ASAN_UBSAN" == true ]]; then
+            export CFLAGS="-fsanitize=address,undefined -g"
+            export ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1
+            export UBSAN_OPTIONS=print_stacktrace=1:print_summary=1:halt_on_error=1
+        fi
+
+        ./bootstrap.sh --enable-tests --prefix=/usr
         make -j"$(nproc)" V=1
         make check VERBOSE=1
         ;;

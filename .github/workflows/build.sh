0cf9b78f1adbeae2206db39a819c281b4b8a1760
Evgeny Vereshchagin <evvers@ya.ru>
2022-12-06 21:33:43 +0000

    CI: get around a build system bug for now
    acx_pthread.m4 is 16 years old and doesn't work with clang and
    ASan/UBSan out of the box. The right fix would most likely be to switch
    to ax_pthread.m4 and tweak the configure script a bit. Until then let's
    just work it around.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -18,6 +18,15 @@ case "$1" in
             export CFLAGS="-fsanitize=address,undefined -g"
             export ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1
             export UBSAN_OPTIONS=print_stacktrace=1:print_summary=1:halt_on_error=1
+
+            # This kludge prevents autoconf from complaining about pthreads. The right fix would be
+            # to replace that out-of-date acx_pthread.m4 with ax_pthread.m4 but it isn't clear how
+            # safe it is to bump autoconf: https://github.com/lathiat/avahi/pull/377. Since it's a
+            # niche use case and nobody else seems to have complained about it so far let's keep the
+            # kludge until some sort of policy covering build dependencies is settled.
+            if [[ "$CC" == clang ]]; then
+                sed -i 's/check_inconsistencies=yes/check_inconsistencies=no/' common/acx_pthread.m4
+            fi
         fi
 
         if ! ./bootstrap.sh --enable-tests --prefix=/usr; then

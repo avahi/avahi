df677c9932645445dfe999d134d5efffed608c5b
Evgeny Vereshchagin <evvers@ya.ru>
2023-05-28 20:31:06 +0000

    ci: turn on coverage reports
    to make it easier to figure out what's covered.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,6 +4,7 @@ set -eux
 set -o pipefail
 
 export ASAN_UBSAN=${ASAN_UBSAN:-false}
+export COVERAGE=${COVERAGE:-false}
 
 case "$1" in
     install-build-deps)
@@ -11,7 +12,7 @@ case "$1" in
         apt-get update -y
         apt-get build-dep -y avahi
         apt-get install -y libevent-dev qtbase5-dev
-        apt-get install -y gcc clang
+        apt-get install -y gcc clang lcov
         ;;
     build)
         if [[ "$ASAN_UBSAN" == true ]]; then
@@ -20,9 +21,21 @@ case "$1" in
             export UBSAN_OPTIONS=print_stacktrace=1:print_summary=1:halt_on_error=1
         fi
 
+        if [[ "$COVERAGE" == true ]]; then
+            export CFLAGS="--coverage"
+        fi
+
         ./bootstrap.sh --enable-tests --prefix=/usr
         make -j"$(nproc)" V=1
         make check VERBOSE=1
+
+        if [[ "$COVERAGE" == true ]]; then
+            lcov --directory . --capture --initial --output-file coverage.info.initial
+            lcov --directory . --capture --output-file coverage.info.run --no-checksum --rc lcov_branch_coverage=1
+            lcov -a coverage.info.initial -a coverage.info.run --rc lcov_branch_coverage=1 -o coverage.info.raw
+            lcov --extract coverage.info.raw "$(pwd)/*" --rc lcov_branch_coverage=1 --output-file coverage.info
+            exit 0
+        fi
         ;;
     *)
         printf '%s' "Unknown command '$1'" >&2

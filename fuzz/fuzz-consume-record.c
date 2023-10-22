311213fffacbf2640bf14c9c1fb98e0c759266c7
Evgeny Vereshchagin <evvers@ya.ru>
2023-10-22 10:36:36 +0000

    fuzz: fuzz more code codepaths processing resource records

--- fuzz/fuzz-consume-record.c
+++ fuzz/fuzz-consume-record.c
@@ -23,12 +23,13 @@
 #include "avahi-common/malloc.h"
 #include "avahi-core/dns.h"
 #include "avahi-core/log.h"
+#include "avahi-core/rr-util.h"
 
 void log_function(AvahiLogLevel level, const char *txt) {}
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     AvahiDnsPacket *p = NULL;
-    AvahiRecord *r = NULL;
+    AvahiRecord *r = NULL, *c = NULL;
     int ret;
 
     avahi_set_log_function(log_function);
@@ -45,9 +46,26 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     ret = avahi_record_is_valid(r);
     assert(ret);
 
+    avahi_record_get_estimate_size(r);
+
     avahi_free(avahi_record_to_string(r));
 
+    if (!(c = avahi_record_copy(r)))
+        goto finish;
+
+    ret = avahi_record_equal_no_ttl(r, c);
+    assert(ret);
+
+    avahi_dns_packet_free(p);
+    if (!(p = avahi_dns_packet_new(size + AVAHI_DNS_PACKET_EXTRA_SIZE)))
+        goto finish;
+
+    if (!avahi_dns_packet_append_record(p, r, 0, 0))
+        goto finish;
+
 finish:
+    if (c)
+        avahi_record_unref(c);
     if (r)
         avahi_record_unref(r);
     if (p)

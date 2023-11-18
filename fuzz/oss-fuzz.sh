35bb1bae902ae9ff2da809fb5f577ba873c43359
Evgeny Vereshchagin <evvers@ya.ru>
2023-11-18 15:59:43 +0000

    fuzz: make it possible to build the fuzz targets without docker
    to make it easier to reproduce bugs locally.

--- fuzz/oss-fuzz.sh
+++ fuzz/oss-fuzz.sh
@@ -17,7 +17,32 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA.
 
+# This script can be run using docker and the OSS-Fuzz toolchain:
+# https://google.github.io/oss-fuzz/advanced-topics/reproducing/#building-using-docker
+#
+# It has to use a few variables like OUT and LIB_FUZZING_ENGINE:
+# https://google.github.io/oss-fuzz/getting-started/new-project-guide/#buildsh
+#
+# The script can also be run locally without the docker machinery by installing
+# clang and running `./fuzz/oss-fuzz.sh`. The fuzz targets are built with
+# ASan/UBSan and put in the out directory.
+
 set -eux
+
+flags="-O1 -fno-omit-frame-pointer -g -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address,undefined -fsanitize=fuzzer-no-link"
+
+export CC=${CC:-clang}
+export CFLAGS=${CFLAGS:-$flags}
+
+export CXX=${CXX:-clang++}
+export CXXFLAGS=${CXXFLAGS:-$flags}
+
+cd "$(dirname -- "$0")/.."
+
+export OUT=${OUT:-"$(pwd)/out"}
+mkdir -p "$OUT"
+
+export LIB_FUZZING_ENGINE=${LIB_FUZZING_ENGINE:--fsanitize=fuzzer}
 
 sed -i 's/check_inconsistencies=yes/check_inconsistencies=no/' common/acx_pthread.m4
 
@@ -31,10 +56,15 @@ make -j"$(nproc)" V=1
 
 for f in fuzz/fuzz-*.c; do
     fuzz_target=$(basename "$f" .c)
+
+    # CFLAGS have to be split
+    # shellcheck disable=SC2086
     $CC -c $CFLAGS -I. \
         "fuzz/$fuzz_target.c" \
         -o "$fuzz_target.o"
 
+    # CXXFLAGS have to be split
+    # shellcheck disable=SC2086
     $CXX $CXXFLAGS \
         "$fuzz_target.o" \
         -o "$OUT/$fuzz_target" \

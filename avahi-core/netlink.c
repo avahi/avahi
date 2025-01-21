0c7d8474b28c4e7da22c37fb29d8546dd37d24f6
Жунёва Мария Михайловна <zhunyova@mts.ru>
2025-01-21 18:48:51 +0300

    Enable Netlink support on FreeBSD
    FreeBSD implements Netlink, but its API resides in a different header files.
    Teach configure.ac to properly locate it on both systems.
    
    Do not try to lookup the message credentials if the system does not support SO_PASSCRED.

--- avahi-core/netlink.c
+++ avahi-core/netlink.c
@@ -50,8 +50,9 @@ int avahi_netlink_work(AvahiNetlink *nl, int block) {
     struct ucred *cred;
     struct iovec iov;
     struct nlmsghdr *p;
+#ifdef SO_PASSCRED
     char cred_msg[CMSG_SPACE(sizeof(struct ucred))];
-
+#endif
     assert(nl);
 
     iov.iov_base = nl->buffer;
@@ -61,8 +62,10 @@ int avahi_netlink_work(AvahiNetlink *nl, int block) {
     smsg.msg_namelen = 0;
     smsg.msg_iov = &iov;
     smsg.msg_iovlen = 1;
+#ifdef SO_PASSCRED
     smsg.msg_control = cred_msg;
     smsg.msg_controllen = sizeof(cred_msg);
+#endif
     smsg.msg_flags = (block ? 0 : MSG_DONTWAIT);
 
     if ((bytes = recvmsg(nl->fd, &smsg, 0)) < 0) {
@@ -74,7 +77,7 @@ int avahi_netlink_work(AvahiNetlink *nl, int block) {
     }
 
     cmsg = CMSG_FIRSTHDR(&smsg);
-
+#ifdef SO_PASSCRED
     if (!cmsg || cmsg->cmsg_type != SCM_CREDENTIALS) {
         avahi_log_warn("No sender credentials received, ignoring data.");
         return -1;
@@ -84,7 +87,7 @@ int avahi_netlink_work(AvahiNetlink *nl, int block) {
 
     if (cred->pid != 0)
         return -1;
-
+#endif
     p = (struct nlmsghdr *) nl->buffer;
 
     assert(nl->callback);
@@ -134,12 +137,12 @@ AvahiNetlink *avahi_netlink_new(const AvahiPoll *poll_api, uint32_t groups, void
         avahi_log_error(__FILE__": bind(): %s", strerror(errno));
         goto fail;
     }
-
+#ifdef SO_PASSCRED
     if (setsockopt(fd, SOL_SOCKET, SO_PASSCRED, &on, sizeof(on)) < 0) {
         avahi_log_error(__FILE__": SO_PASSCRED: %s", strerror(errno));
         goto fail;
     }
-
+#endif
     if (!(nl = avahi_new(AvahiNetlink, 1))) {
         avahi_log_error(__FILE__": avahi_new() failed.");
         goto fail;
--- avahi-core/netlink.h
+++ avahi-core/netlink.h
@@ -19,13 +19,17 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
   USA.
 ***/
-
+#ifdef __linux__
 #include <sys/socket.h>
 #include <asm/types.h>
 #include <inttypes.h>
 
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
+#else
+#include <netlink/netlink.h>
+#include <netlink/netlink_route.h>
+#endif
 
 #include <avahi-common/watch.h>
 

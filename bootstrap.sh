d8dc971ce29737617c788791dd605930e9ed8072
Жунёва Мария Михайловна <zhunyova@mts.ru>
2025-02-04 17:58:33 +0300

    add CI for FreeBSD

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,7 @@
 # USA.
 
 FLAGS="--sysconfdir=/etc --localstatedir=/var --enable-tests --enable-compat-howl --enable-compat-libdns_sd"
+export MAKE="make"
 
 # Feel free to add your own custom flags in here -Lathiat
 
@@ -30,13 +31,12 @@ case `uname -s` in
     FLAGS="$FLAGS --prefix=/opt/local --disable-pygtk"
     ;;
     FreeBSD)
-    cp /usr/local/share/aclocal/libtool15.m4 common
-    cp /usr/local/share/aclocal/pkg.m4 common
-    export LIBTOOLIZE=/usr/local/bin/libtoolize15
+    export MAKE="gmake"
+    export LIBTOOLIZE=/usr/local/bin/libtoolize
     export CFLAGS="-I/usr/local/include"
-    export LDFLAGS="-L/usr/local/lib"
+    export LDFLAGS="-L/usr/local/lib -lintl"
     export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
-    FLAGS="$FLAGS --prefix=/opt/ --with-distro=none --disable-python --disable-dbus --disable-glib --disable-gtk --disable-libevent"
+    FLAGS="$FLAGS --prefix=/opt/ --with-distro=freebsd --disable-libsystemd --disable-mono --disable-manpages --disable-libevent"
     ;;
     NetBSD)
     export LIBTOOLIZE=libtoolize

e62e367bb69ba0c835d28952ae45d285d6ca1166
Petr Menšík <pemensik@fedoraproject.org>
2025-10-11 21:53:42 +0200

    Modify cname-test to run all tests by just simple run
    Create a table of tests, set expected hostname. For tests which should
    pass, use hostname of machine itself as final cname target.

--- avahi-core/cname-test.c
+++ avahi-core/cname-test.c
@@ -48,6 +48,12 @@ static void quit_timeout_callback(AVAHI_GCC_UNUSED AvahiTimeout *timeout, void *
     avahi_simple_poll_quit(simple_poll);
 }
 
+typedef struct {
+	const char *name;
+	void (*test_case)(void);
+	AvahiResolverEvent expected;
+} test_cases_db_t;
+
 #define avahi_test_cache_flush() avahi_cache_flush(cache)
 
 static void avahi_test_add_a(const char *src, const char *dst, uint32_t ttl) {
@@ -81,6 +87,11 @@ static void avahi_test_add_cname(const char *src, const char *dst) {
     avahi_log_debug("Added CNAME record to cache: %s -> %s", src, dst);
 }
 
+static const char *myself(void) {
+	assert(server);
+	return avahi_server_get_host_name_fqdn(server);
+}
+
 static void self_loop(void) {
     avahi_test_add_cname("X.local", "X.local");
 }
@@ -92,7 +103,7 @@ static void retransmit_cname(void) {
 }
 
 static void one_normal(void) {
-    avahi_test_add_cname("X.local", "Y.local");
+    avahi_test_add_cname("X.local", myself());
 }
 
 static void one_loop(void) {
@@ -102,7 +113,7 @@ static void one_loop(void) {
 
 static void two_normal(void) {
     avahi_test_add_cname("X.local", "Y.local");
-    avahi_test_add_cname("Y.local", "Z.local");
+    avahi_test_add_cname("Y.local", myself());
 }
 
 static void two_loop(void) {
@@ -126,7 +137,7 @@ static void two_loop_inner2(void) {
 static void three_normal(void) {
     avahi_test_add_cname("X.local", "Y.local");
     avahi_test_add_cname("Y.local", "Z.local");
-    avahi_test_add_cname("Z.local", "A.local");
+    avahi_test_add_cname("Z.local", myself());
 }
 
 static void three_loop(void) {
@@ -153,6 +164,23 @@ static void cname_answer_diamond(void) {
 
 static void cname_answer(void) {
     avahi_test_add_cname("X.local", "Y.local");
+    avahi_test_add_a("Y.local", "192.168.50.99", 2);
+}
+
+/* this test should be limited by number of queries per test. */
+static void cname_toomuch(void) {
+    unsigned i;
+    char from[20];
+    char to[20];
+
+    avahi_test_add_cname("X.local", "C1.local");
+    for (i=1; i<25; i++) {
+	snprintf(from, sizeof(from), "C%d.local", i);
+	snprintf(to, sizeof(to), "C%d.local", i+1);
+	avahi_test_add_cname(from, to);
+    }
+    snprintf(from, sizeof(from), "C%d.local", i);
+    avahi_test_add_cname(from, "Y.local");
     avahi_test_add_a("Y.local", "192.168.50.99", 2);
 }
 
@@ -197,46 +225,61 @@ static void hnr_callback(
 
     avahi_log_debug("HNR: (%i.%i) <%s> -> %s [%s]", iface, protocol, hostname, a ? t : "n/a",
                     resolver_event_to_string(event));
+    *((AvahiResolverEvent *) userdata) = event;
 }
 
-#define CHECK_TEST_CASE(test_name, function) \
-    if (!strcmp(test_case, test_name)) \
-        avahi_test_case_function = &function;
+static const test_cases_db_t test_cases[] = {
+    {"self_loop", self_loop, AVAHI_RESOLVER_FAILURE},
+    {"retransmit_cname", retransmit_cname, AVAHI_RESOLVER_FAILURE},
+    {"one_normal", one_normal, AVAHI_RESOLVER_FOUND},
+    {"one_loop", one_loop, AVAHI_RESOLVER_FAILURE},
+    {"two_normal", two_normal, AVAHI_RESOLVER_FOUND},
+    {"two_loop", two_loop, AVAHI_RESOLVER_FAILURE},
+    {"two_loop_inner", two_loop_inner, AVAHI_RESOLVER_FAILURE},
+    {"two_loop_inner2", two_loop_inner2, AVAHI_RESOLVER_FAILURE},
+    {"three_normal", three_normal, AVAHI_RESOLVER_FOUND},
+    {"three_loop", three_loop, AVAHI_RESOLVER_FAILURE},
+    {"diamond", diamond, AVAHI_RESOLVER_FAILURE},
+    {"cname_answer_diamond", cname_answer_diamond, AVAHI_RESOLVER_FOUND},
+    {"cname_answer", cname_answer, AVAHI_RESOLVER_FOUND},
+    {"cname_toomuch", cname_toomuch, AVAHI_RESOLVER_FAILURE},
+    {NULL, NULL, AVAHI_RESOLVER_FAILURE},
+};
 
-static void avahi_test_initialize(char *test_case) {
+static void test_list(void) {
+    const test_cases_db_t *c;
+
+    for (c=test_cases; c->name; c++) {
+	    printf("%s\n", c->name);
+    }
+}
+
+static const test_cases_db_t * avahi_test_initialize(const char *test_case) {
+    const test_cases_db_t *c;
+
     assert(test_case);
 
-    CHECK_TEST_CASE("self_loop", self_loop);
-    CHECK_TEST_CASE("retransmit_cname", retransmit_cname);
-    CHECK_TEST_CASE("one_normal", one_normal);
-    CHECK_TEST_CASE("one_loop", one_loop);
-    CHECK_TEST_CASE("two_normal", two_normal);
-    CHECK_TEST_CASE("two_loop", two_loop);
-    CHECK_TEST_CASE("two_loop_inner", two_loop_inner);
-    CHECK_TEST_CASE("two_loop_inner2", two_loop_inner2);
-    CHECK_TEST_CASE("three_normal", three_normal);
-    CHECK_TEST_CASE("three_loop", three_loop);
-    CHECK_TEST_CASE("diamond", diamond);
-    CHECK_TEST_CASE("cname_answer_diamond", cname_answer_diamond);
-    CHECK_TEST_CASE("cname_answer", cname_answer);
-
-    assert(avahi_test_case_function);
+    for (c=test_cases; c->name; c++) {
+	if (!strcmp(test_case, c->name)) {
+	    return c;
+	}
+    }
+    return NULL;
 }
 
-int main(int argc, char *argv[]) {
-    int error;
+/* Run one single test case.
+ *
+ * Return 0 when ok, non-zero on error. */
+static int run_single(const test_cases_db_t *test) {
+    int error = 0;
     struct timeval tv;
-
     AvahiSHostNameResolver *hnr;
     AvahiServerConfig config;
     AvahiSimplePoll *simple_poll;
+    AvahiResolverEvent revent = AVAHI_RESOLVER_FAILURE;
 
-    if (argc < 2) {
-        printf("Usage: %s test_name", argv[0]);
-        return 1;
-    }
-
-    avahi_test_initialize(argv[1]);
+    avahi_test_case_function = test->test_case;
+    avahi_log_info("# Starting test %s\n", test->name);
 
     simple_poll = avahi_simple_poll_new();
     poll_api = avahi_simple_poll_get(simple_poll);
@@ -249,7 +292,7 @@ int main(int argc, char *argv[]) {
     avahi_cache_flush(cache);
 
     hnr = avahi_s_host_name_resolver_prepare(server, AVAHI_IF_UNSPEC, AVAHI_PROTO_UNSPEC, "X.local", AVAHI_PROTO_UNSPEC,
-                                             AVAHI_LOOKUP_USE_MULTICAST, hnr_callback, NULL);
+                                             AVAHI_LOOKUP_USE_MULTICAST, hnr_callback, &revent);
     avahi_s_host_name_resolver_start(hnr);
 
     avahi_elapse_time(&tv, 1000 * 5, 0);
@@ -259,13 +302,55 @@ int main(int argc, char *argv[]) {
 
     avahi_s_host_name_resolver_free(hnr);
 
-    avahi_log_info("Test complete!");
+    error = (revent != test->expected);
+    avahi_log_info("%s: %s: test complete, result: %s, expected: %s!",
+		    error ? "FAIL" : "PASS",
+		    test->name,
+		    resolver_event_to_string(revent),
+		    resolver_event_to_string(test->expected));
     if (server) {
         avahi_server_free(server);
     }
 
     if (simple_poll) {
         avahi_simple_poll_free(simple_poll);
+    }
+    return error;
+}
+
+static int run_all(void) {
+    const test_cases_db_t *c;
+    int r = 0;
+    int i = 0;
+
+    for (c=test_cases; c->name; c++) {
+	    i++;
+	    if (run_single(c) != 0)
+		r++;
+    }
+    avahi_log_info("# All %d test cases finished, status: %d", i, r);
+    return r;
+}
+
+
+int main(int argc, char *argv[]) {
+    const test_cases_db_t *test;
+
+    if (argc < 2) {
+        printf("Usage: %s [--list] [test_name]\n", argv[0]);
+	return run_all();
+    }
+
+    if (!strcmp(argv[1], "--list")) {
+	test_list();
+	return 0;
+    }
+    test = avahi_test_initialize(argv[1]);
+    if (test) {
+        return run_single(test);
+    } else {
+	printf("Test not found: %s\n", argv[1]);
+	return 2;
     }
 
     return 0;

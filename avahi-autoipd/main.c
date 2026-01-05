148925a4ffa6bbb45b43aa3c8df296139f1ec287
Damian Kaczka <damiankaczka@outlook.com>
2026-01-05 09:04:51 +0100

    avahi-autoipd: ignore and log read errors from RANDOM_DEVICE
    It's safe to continue when read() from RANDOM_DEVICE fails, since
    a failure is accounted for by XORing time() to the seed. This is
    mentioned explicitly in the following code.

--- avahi-autoipd/main.c
+++ avahi-autoipd/main.c
@@ -186,7 +186,8 @@ static void init_rand_seed(void) {
      * less predictable, and to make sure that multiple machines
      * booted at the same time choose different random seeds.  */
     if ((fd = open(RANDOM_DEVICE, O_RDONLY)) >= 0) {
-        read(fd, &seed, sizeof(seed));
+        if (read(fd, &seed, sizeof(seed)) <= 0)
+            daemon_log(LOG_WARNING, "read() from \"%s\" failed: %s", RANDOM_DEVICE, strerror(errno));
         close(fd);
     }
 
